10f0101b189cbf8ae79c52ed19b43507b757bc3a
gyubum <sgb8170@naver.com>
2022-01-06 20:02:11 +0900

    inital commit

---
+++ Naetube/src/main/java/Youtube/Naetube/NaetubeApplication.java
@@ -0,0 +1,13 @@
+package Youtube.Naetube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaetubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaetubeApplication.class, args);
+	}
+
+}
---
+++ Naetube/src/test/java/Youtube/Naetube/NaetubeApplicationTests.java
@@ -0,0 +1,13 @@
+package Youtube.Naetube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaetubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
