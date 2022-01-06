cb66ea572d4f223cad24e8373b4ce378809a6c42
gyubum <sgb8170@naver.com>
2022-01-06 20:13:57 +0900

    test commit pycharm

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+# test python
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
