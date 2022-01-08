f0cd3be4d3d04778d3b31caf2ee5ac95bcbee643
KimKyungJi <kkj2551@naver.com>
2022-01-08 17:12:06 +0900

    [chore] test

--- Naetube/src/main/java/Youtube/Naetube/NaetubeApplication.java
+++ Naetube/src/main/java/Youtube/Naetube/NaetubeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NaetubeApplication {
 
 	public static void main(String[] args) {
-
+		//start
 		SpringApplication.run(NaetubeApplication.class, args);
 	}
 
