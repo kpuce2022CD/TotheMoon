2d93a9b544ed91c8df4faf9b48ab12c1100e9aea
KimKyungJi <kkj2551@naver.com>
2022-01-09 06:30:36 +0900

    [feat] 검색 페이지 추가

--- Naetube/src/main/java/Youtube/Naetube/controller/HomeController.java
+++ Naetube/src/main/java/Youtube/Naetube/controller/HomeController.java
@@ -1,7 +1,9 @@
 package Youtube.Naetube.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -9,6 +11,12 @@ public class HomeController {
     @GetMapping("/")
     public String Home(){
         return "home";
+    }
+
+    @GetMapping("/search/{url}")
+    public String Search(@PathVariable String url, Model model){
+        model.addAttribute("url", "https://www.youtube.com/embed/"+url);
+        return "search";
     }
 
 }
