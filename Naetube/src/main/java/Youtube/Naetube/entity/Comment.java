5b20c423c2f7f5989f20e745ec73ce8ce949ca4c
gyubum <sgb8170@naver.com>
2022-01-20 01:54:13 +0900

    test commit

---
+++ Naetube/src/main/java/Youtube/Naetube/entity/Comment.java
@@ -0,0 +1,16 @@
+package Youtube.Naetube.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Comment {
+    private String index;
+    private String id;
+    private String comment;
+    private String date;
+    private String num_like;
+}
