1b208f1fd9cb6e650c39c9dc9baf17a0495ef31d
KimKyungJi <kkj2551@naver.com>
2022-01-28 03:42:18 +0900

    [fix] 데이터 누적 이슈 해결

--- ApiServer/np_classify.py
+++ ApiServer/np_classify.py
@@ -30,59 +30,19 @@ with open('np_tokenizer.pickle', 'rb') as handle:
 
 loaded_model = load_model('np_classify_model.h5')
 
-global comment_dic
-comment_dic = {}  # 추출한 댓글 데이터
-global author_dic
-author_dic = {}  # 추출한 작성자 데이터
-global date_dic
-date_dic = {}  # 추출한 작성 날짜 데이터
-global num_likes_dic
-num_likes_dic = {}  # 추출한 좋아요 개수 데이터
-global comment_result
-comment_result = []  # 전처리 후 데이터
-global author_result
-author_result = []  # 전처리 후 데이터
-global date_result
-date_result = []  # 전처리 후 데이터
-global num_likes_result
-num_likes_result = []  # 전처리 후 데이터
 
-global show_comment
-show_comment= []
-
-global dic_return  # 스프링으로 전송할 json 데이터
-dic_return = []
-
-global dic_temp  # json 배열 추가를 위한 버퍼 역할
-dic_temp = {}
 def comment_classify(comment_file):
     print("dd")
 
 
-def sentiment_predict(new_sentence, id, comment, date, num_like):
-    global dic_return
-    global dic_temp
-
+def sentiment_predict(new_sentence):
     new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]', '', new_sentence)
     new_sentence = okt.morphs(new_sentence, stem=True)  # 토큰화
     new_sentence = [word for word in new_sentence if not word in stopwords]  # 불용어 제거
     encoded = tokenizer.texts_to_sequences([new_sentence])  # 정수 인코딩
     pad_new = pad_sequences(encoded, maxlen=max_len)  # 패딩
     score = float(loaded_model.predict(pad_new))  # 예측
-    if (score > 0.5):
-        if(score > 0.8):
-            print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
-            dic_temp = {"index": "1", "id": id, "comment": comment,
-                        "date": date, "num_like": num_like}
-            dic_return.append(dic_temp)
-
-    else:
-        if(score < 0.2):
-            print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
-            dic_temp = {"index": "0", "id": id, "comment": comment,
-                        "date": date, "num_like": num_like}
-            dic_return.append(dic_temp)
-
+    return score
 
 # 이모티콘 제거 함수(ASCII 코드에 해당하지 않는 경우)
 def remove_emoji(inputString):
@@ -136,44 +96,38 @@ def emoticonToWord(comment):
 def youtube_comment_processing(filename):
     df = pd.read_excel(filename)
 
-    # 엑셀 파일로부터 데이터 추출
-    for i in df.index:
-        comment = df.loc[i, 'comment']  # 댓글 내용
-        author = df.loc[i, 'author']  # 작성자
-        date = df.loc[i, 'date']  # 작성 날짜
-        num_likes = df.loc[i, 'num_likes']  # 좋아요 개수
-        comment_dic[i] = comment  # comment_dic 딕셔너리에 추가
-        author_dic[i] = author  # author_dic 딕셔너리에 추가
-        date_dic[i] = date  # date_dic 딕셔너리에 추가
-        num_likes_dic[i] = num_likes  # num_likes 딕셔너리에 추가
+    comments = df.values    # 댓글 데이터(댓글, 작성자, 날짜, 좋아요 수)
+    comment_result = []  # 전처리 후 데이터
+    dic_return = [] # 스프링으로 전송할 json 데이터
 
-    for val in comment_dic.values():
-        print(val)  # 댓글 원본
+    for i in comments:
+        val = i[0]
+        print(val)
         val = re.sub("<br>", " ", val)  # <br> 한줄띄기 -> 스페이스 공백으로 변환 , 제거 이모티콘 추가
-        show_comment.append(val)
         val = emoticonToWord(val)  # 이모티콘 텍스트로 변환
         val = re.sub(emoji_pattern, "", val)  # 이모티콘 제거
         remove_emoji(val) #이모티콘 제거
         comment_result.append(val)
 
-    for val in author_dic.values():  # 작성자 배열에 추가
-        author_result.append(val)
-
-    for val in date_dic.values():  # 작성 날짜 배열에 추가
-        date_result.append(val)
-
-    for val in num_likes_dic.values():  # 좋아요 개수 배열에 추가
-        num_likes_result.append(val)
-
     for i in range(len(comment_result)):  # 배열 크기만큼 실행
         print("------------------전처리 후-----------------")
-        print("댓글: " + comment_result[i])
-        print("작성자: " + author_result[i])
-        print("작성 날짜: " + date_result[i])
-        print("좋아요 개수: " + str(num_likes_result[i]))
-        sentiment_predict(comment_result[i], author_result[i], show_comment[i], date_result[i], str(num_likes_result[i]))
+        print("댓글: " + comments[i][0])
+        print("작성자: " + comments[i][1])
+        print("작성 날짜: " + comments[i][2])
+        print("좋아요 개수: " + str(comments[i][3]))
+        score = sentiment_predict(comment_result[i])
+        if (score > 0.5):
+            if(score > 0.8):
+                print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+                dic_temp = {"index": "1", "id": comments[i][1], "comment": comments[i][0],
+                            "date": comments[i][2], "num_like": str(comments[i][3])}
+                dic_return.append(dic_temp)
+        else:
+            if(score < 0.2):
+                print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
+                dic_temp = {"index": "0", "id": comments[i][1], "comment": comments[i][0],
+                            "date": comments[i][2], "num_like": str(comments[i][3])}
+                dic_return.append(dic_temp)
 
     return dic_return
 
-#youtube_comment_processing("test.xlsx")
-#print(dic_return)
