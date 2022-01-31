8d806341df1683a65192ca4350acccda2c16f493
gyubum <sgb8170@naver.com>
2022-01-31 21:16:30 +0900

    em_classify.py 업로드, requirements.txt commit

---
+++ ApiServer/em_classify.py
@@ -0,0 +1,136 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+# kobert
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+# transformers
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+# for commit
+# GPU 사용
+device = torch.device("cuda:0")
+
+# BERT 모델, Vocabulary 불러오기
+bertmodel, vocab = get_pytorch_kobert_model()
+
+# 토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+# Setting parameters
+max_len = 64
+batch_size = 1
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-5
+num_workers = 0
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i],))
+
+    def __len__(self):
+        return (len(self.labels))
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size=768,
+                 num_classes=7,  ##클래스 수 조정##
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+
+        self.classifier = nn.Linear(hidden_size, num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+        _, pooler = self.bert(input_ids=token_ids, token_type_ids=segment_ids.long(),
+                              attention_mask=attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+
+model = torch.load('em_classify_model.pt')
+
+
+def predict(predict_sentence):
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=0)
+
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length = valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        test_eval = []
+        for i in out:
+            logits = i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("공포가")
+            elif np.argmax(logits) == 1:
+                test_eval.append("놀람이")
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노가")
+            elif np.argmax(logits) == 3:
+                test_eval.append("슬픔이")
+            elif np.argmax(logits) == 4:
+                test_eval.append("중립이")
+            elif np.argmax(logits) == 5:
+                test_eval.append("행복이")
+            elif np.argmax(logits) == 6:
+                test_eval.append("혐오가")
+
+        print(">> 입력하신 내용에서 " + test_eval[0] + " 느껴집니다.")
+
+
+# 질문 무한반복하기! 0 입력시 종료
+end = 1
+while end == 1:
+    sentence = input("하고싶은 말을 입력해주세요 : ")
+    if sentence == 0:
+        break
+    predict(sentence)
+    print("\n")
