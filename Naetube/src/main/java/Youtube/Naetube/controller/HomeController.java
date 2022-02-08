b661d448e597431bc46c9c8b2f7c1260c7bbe946
gyubum <sgb8170@naver.com>
2022-02-08 16:01:43 +0900

    html 테스트 코드 추가

--- Naetube/src/main/java/Youtube/Naetube/controller/HomeController.java
+++ Naetube/src/main/java/Youtube/Naetube/controller/HomeController.java
@@ -81,6 +81,8 @@ public class HomeController {
         System.out.println("혐오 댓글 비율 = " + return_disgustPercent);
 
 
+
+
         // "행복" 댓글 출력 테스트
         //for(int i=0;i<happyComments.size();i++){
         //    System.out.println(happyComments.get(i).getComment());
@@ -137,13 +139,131 @@ public class HomeController {
         return "timeline";
     }
 
+    @GetMapping("/test/{url}")
+    public String test_search(@PathVariable String url, Model model){
+
+
+        Comment[] comments = new Comment[900];
+
+        for(int i=0;i<100;i++){
+            comments[i] = new Comment("0","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+        }
+        for(int i=100;i<200;i++){
+            comments[i] = new Comment("1","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=200;i<300;i++){
+            comments[i] = new Comment("2","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=300;i<400;i++){
+            comments[i] = new Comment("3","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=400;i<500;i++){
+            comments[i] = new Comment("4","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=500;i<600;i++){
+            comments[i] = new Comment("5","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=600;i<700;i++){
+            comments[i] = new Comment("6","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=700;i<800;i++){
+            comments[i] = new Comment("7","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+        for(int i=800;i<900;i++){
+            comments[i] = new Comment("8","testId","테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.","2020-20-20","5");
+
+        }
+
+        List<Comment> positiveComments = new ArrayList<>(); // json 구분 인덱스 : 1
+        List<Comment> negativeComments = new ArrayList<>(); // json 구분 인덱스 : 0
+        List<Comment> fearComments = new ArrayList<>(); // json 구분 인덱스 : 2
+        List<Comment> surprisedComments = new ArrayList<>(); // json 구분 인덱스 : 3
+        List<Comment> angerComments = new ArrayList<>(); // json 구분 인덱스 : 4
+        List<Comment> sadnessComments = new ArrayList<>(); // json 구분 인덱스 : 5
+        List<Comment> neutralComments = new ArrayList<>(); // json 구분 인덱스 : 6
+        List<Comment> happyComments = new ArrayList<>(); // json 구분 인덱스 : 7
+        List<Comment> disgustComments = new ArrayList<>(); // json 구분 인덱스 : 8
+
+        //댓글 분류
+        classifyComment(comments, positiveComments, negativeComments, fearComments, surprisedComments, angerComments, sadnessComments, neutralComments, happyComments, disgustComments);
+
+
+        System.out.println("positiveComments = " + positiveComments);
+        //[Comment(index=1, id=상휘1퍼센트, comment=첫곡 미쳤다, date=2022-01-19T04:29:36Z, num_like=0) , ...]
+        System.out.println("positiveComments[0] = " + positiveComments.get(0));
+        //Comment(index=1, id=상휘1퍼센트, comment=첫곡 미쳤다, date=2022-01-19T04:29:36Z, num_like=0)
+
+        double positivePercent = ((double)positiveComments.size() / ((double)positiveComments.size()+(double)negativeComments.size()))*100;
+        double negativePercent = ((double)negativeComments.size() / ((double)positiveComments.size()+(double)negativeComments.size()))*100;
+        double emTotalSize = fearComments.size()+ surprisedComments.size()+ angerComments.size()+ sadnessComments.size()+ neutralComments.size()+ happyComments.size()+ disgustComments.size();
+        double fearPercent = (((double)fearComments.size()/emTotalSize)*100);
+        double surprisedPercent = ((double)surprisedComments.size()/emTotalSize)*100;
+        double angerPercent = ((double)angerComments.size()/emTotalSize)*100;
+        double sadnessPercent = ((double)sadnessComments.size()/emTotalSize)*100;
+        double neutralPercent = ((double)neutralComments.size()/emTotalSize)*100;
+        double happyPercent = ((double)happyComments.size()/emTotalSize)*100;
+        double disgustPercent = ((double)disgustComments.size()/emTotalSize)*100;
+
+        // 소수점 정규화
+        double return_fearPercent = Double.parseDouble(String.format("%.1f",fearPercent));
+        double return_surprisedPercent = Double.parseDouble(String.format("%.1f",surprisedPercent));
+        double return_angerPercent = Double.parseDouble(String.format("%.1f",angerPercent));
+        double return_sadnessPercent = Double.parseDouble(String.format("%.1f",sadnessPercent));
+        double return_neutralPercent = Double.parseDouble(String.format("%.1f",neutralPercent));
+        double return_happyPercent = Double.parseDouble(String.format("%.1f",happyPercent));
+        double return_disgustPercent = Double.parseDouble(String.format("%.1f",disgustPercent));
+
+
+
+        System.out.println("전체 댓글 수 = " + comments.length);
+        System.out.println("긍정 댓글 수 = " + positiveComments.size());
+        System.out.println("부정 댓글 수  = " + negativeComments.size());
+        System.out.println("긍정 댓글 비율 = " + positivePercent);
+        System.out.println("부정 댓글 비율 = " + negativePercent);
+        System.out.println("공포 댓글 비율 = " + return_fearPercent);
+        System.out.println("놀람 댓글 비율 = " + return_surprisedPercent);
+        System.out.println("분노 댓글 비율 = " + return_angerPercent);
+        System.out.println("슬픔 댓글 비율 = " + return_sadnessPercent);
+        System.out.println("중립 댓글 비율 = " + return_neutralPercent);
+        System.out.println("행복 댓글 비율 = " + return_happyPercent);
+        System.out.println("혐오 댓글 비율 = " + return_disgustPercent);
+
+
+        // "행복" 댓글 출력 테스트
+        //for(int i=0;i<happyComments.size();i++){
+        //    System.out.println(happyComments.get(i).getComment());
+        //}
+
+        model.addAttribute("url", "https://www.youtube.com/embed/"+url);    //search.html에 url 전달.
+        model.addAttribute("positiveComments",positiveComments);
+        model.addAttribute("negativeComments",negativeComments);
+        model.addAttribute("comments",comments);
+        model.addAttribute("positivePercent", positivePercent);
+        model.addAttribute("negativePercent", negativePercent);
+        model.addAttribute("fearPercent",return_fearPercent);
+        model.addAttribute("surprisedPercent",return_surprisedPercent);
+        model.addAttribute("angerPercent",return_angerPercent);
+        model.addAttribute("sadnessPercent",return_sadnessPercent);
+        model.addAttribute("neutralPercent",return_neutralPercent);
+        model.addAttribute("happyPercent",return_happyPercent);
+        model.addAttribute("disgustPercent",return_disgustPercent);
+        return "test_search";
+    }
+
 
     //댓글 분류
     private void classifyComment(Comment[] comments, List<Comment> positiveComments, List<Comment> negativeComments, List<Comment> fearComments,
                                  List<Comment> surprisedComments, List<Comment> angerComments, List<Comment> sadnessComments, List<Comment> neutralComments,
                                  List<Comment> happyComments, List<Comment> disgustComments) {
         for(int i = 0; i< comments.length; i++){    //인덱스 번호를 통해서 긍정, 부정 , 감정 댓글 분류
-            if(comments[i].getIndex().equals("1")){
+            if(comments[i].getIndex().equals("1")) {
                 positiveComments.add(comments[i]);
             }
             else if(comments[i].getIndex().equals("2")){
--- Naetube/src/main/java/Youtube/Naetube/entity/Comment.java
+++ Naetube/src/main/java/Youtube/Naetube/entity/Comment.java
@@ -15,4 +15,12 @@ public class Comment {
     private String comment;
     private String date;
     private String num_like;
+
+    public Comment(String index, String id, String comment, String date, String num_like) {
+        this.index = index;
+        this.id = id;
+        this.comment = comment;
+        this.date = date;
+        this.num_like = num_like;
+    }
 }
