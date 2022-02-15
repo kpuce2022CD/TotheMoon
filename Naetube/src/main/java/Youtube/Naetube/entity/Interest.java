a98392d74bc06d357c321b64f3c458322032de99
gyubum <sgb8170@naver.com>
2022-02-16 00:28:04 +0900

    Interest entity commit

---
+++ Naetube/src/main/java/Youtube/Naetube/entity/Interest.java
@@ -0,0 +1,19 @@
+package Youtube.Naetube.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Interest {
+    private String commentDate;
+    private String commentCount;
+
+    public Interest(@JsonProperty("commentDate") String commentDate, @JsonProperty("commentCount") String commentCount) {
+        this.commentDate = commentDate;
+        this.commentCount = commentCount;
+    }
+}
