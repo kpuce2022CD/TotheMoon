8acfe8f0b0be2b71735278940581f3456c7d0b77
KimKyungJi <kkj2551@naver.com>
2022-05-21 18:41:38 +0900

    [feat] 타임라인 count 툴팁 추가

--- frontend/src/components/Timeline/Timeline.js
+++ frontend/src/components/Timeline/Timeline.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Timeline.css";
+import ReactTooltip from "react-tooltip";
 
-const Timeline = ({ clickfunc, children }) => {
-  return <span className="timeline" onClick={clickfunc}>{children}</span>;
+const Timeline = ({ clickfunc, children, count }) => {
+  return (
+    <>
+      <span className="timeline" onClick={clickfunc} data-tip={count}>
+        {children}
+      </span>
+      <ReactTooltip />
+    </>
+  );
 };
 
 export default Timeline;
