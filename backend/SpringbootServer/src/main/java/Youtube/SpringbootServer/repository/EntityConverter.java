c8af136f9b3cb0253ed8c4b3301907f306fc5ff4
joonhyuck <jh83370@naver.com>
2022-06-30 01:43:56 +0900

    keywordDTO 변환

--- backend/SpringbootServer/src/main/java/Youtube/SpringbootServer/repository/EntityConverter.java
+++ backend/SpringbootServer/src/main/java/Youtube/SpringbootServer/repository/EntityConverter.java
@@ -1,7 +1,10 @@
 package Youtube.SpringbootServer.repository;
 
 import Youtube.SpringbootServer.dto.CommentDTO;
+import Youtube.SpringbootServer.dto.KeywordDTO;
 import Youtube.SpringbootServer.entity.Comment;
+import Youtube.SpringbootServer.entity.Keyword;
+import Youtube.SpringbootServer.entity.KeywordComment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,5 +12,13 @@ public class EntityConverter {
 
     public Comment toCommentEntity(CommentDTO commentDTO){
         return new Comment(commentDTO.getId(), commentDTO.getDate(), commentDTO.getIndex() , commentDTO.getComment(), commentDTO.getNum_like());
+    }
+
+    public Keyword toKeywordEntity(KeywordDTO keywordDTO, int i){
+        return new Keyword(i, keywordDTO.getB5()[i]);
+    }
+
+    public KeywordComment toKeywordCommentEntity(KeywordDTO keywordDTO,int i, int j){
+        return new KeywordComment(keywordDTO.getComments()[i][j]);
     }
 }
