01a715a4c6a2be1aa18f833627023d93a9459173
joonhyuck <jh83370@naver.com>
2022-06-30 01:39:04 +0900

    BoardController

---
+++ backend/SpringbootServer/src/main/java/Youtube/SpringbootServer/controller/BoardController.java
@@ -0,0 +1,57 @@
+package Youtube.SpringbootServer.controller;
+
+import Youtube.SpringbootServer.dto.CommentListDTO;
+import Youtube.SpringbootServer.dto.KeywordDTO;
+import Youtube.SpringbootServer.entity.Comment;
+import Youtube.SpringbootServer.entity.Record;
+import Youtube.SpringbootServer.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+    private final CommentListDTO commentListDTO;
+    private final KeywordDTO keywordDTO;
+
+    //목록 조회
+    @GetMapping("/list")
+    public String recordList(Model model){
+        List<Record> records = boardService.findRecords();
+        model.addAttribute("records", records);
+        return "db_complete_list";
+    }
+
+    //저장
+    @GetMapping("/persist")
+    public String persistComment(){
+        Record record = new Record();
+        System.out.println("/////////////////////keywordDTO = " + keywordDTO.getB5()[0]);
+        boardService.registerDB(commentListDTO,record,keywordDTO);
+        return "redirect:/list";
+    }
+
+    //분석 1건 조회
+    @GetMapping("/persist/{recordId}")
+    public String showComments(@PathVariable String recordId,  Model model){
+        long longRecordId = Long.parseLong(recordId);
+        List<Comment> comments = boardService.findComment(longRecordId);
+        model.addAttribute("comments",comments);
+        return "db_complete_show";
+    }
+
+    //분석 1건 삭제
+    @GetMapping("/delete/{recordId}")
+    public String delete(@PathVariable String recordId){
+        long longRecordId = Long.parseLong(recordId);
+        boardService.delete(longRecordId);
+        return "redirect:/list";
+    }
+}
