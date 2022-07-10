4c5d53ef6df38f0c580ecf789a5fe14a08f98a2e
joonhyuck <jh83370@naver.com>
2022-07-10 22:51:02 +0900

    프론트엔드 버튼 추가

--- frontend/src/components/Analyze/Analyze.js
+++ frontend/src/components/Analyze/Analyze.js
@@ -179,6 +179,12 @@ const Analyze = () => {
                   :
                   <li style={{margin:"10px"}} class="nav-item" onClick={() => {setClickedMenuTab(4)}}><a style={{color:"white"}} class="nav-link js-scroll-trigger" href="http://localhost:8080/persist" onclick="alert('저장 완료')" target="_blank">분석 결과 저장</a></li>
                 }
+                {
+                  clickedMenuTab===5 ? 
+                  <li style={{backgroundColor:"rgb(250,128,114)",margin:"10px"}} class="nav-item" onClick={() => {setClickedMenuTab(5)}}><a style={{color:"white"}} class="nav-link js-scroll-trigger" href="http://localhost:8080/memberinfo" target="_blank">MY PAGE</a></li>
+                  :
+                  <li style={{margin:"10px"}} class="nav-item" onClick={() => {setClickedMenuTab(4)}}><a style={{color:"white"}} class="nav-link js-scroll-trigger" href="http://localhost:8080/memberinfo" target="_blank">MY PAGE</a></li>
+                }
               </ul>
             </div>
           </nav>
